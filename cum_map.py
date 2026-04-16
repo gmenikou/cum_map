3b2ee2f7a697bd9ff5067c6900e83a1a312d0a92
gmenikou <geo.menikou@shso.org.cy>
2026-04-16 13:42:32 +0300

    Update cum_map.py

--- cum_map.py
+++ cum_map.py
@@ -430,7 +430,7 @@ if uploaded_files:
             csv_lines.append(f"pixels_ge_2gy,{thr2}")
             csv_lines.append(f"pixels_ge_5gy,{thr5}")
             csv_lines.append(f"pixels_ge_10gy,{thr10}")
-            csv_bytes = "
+            csv_bytes = "\n".join(csv_lines).encode("utf-8")
 ".join(csv_lines).encode("utf-8")
 
             npy_buf = io.BytesIO()
